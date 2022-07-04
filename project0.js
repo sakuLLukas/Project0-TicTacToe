d2db31508e6eadf74f1f1b306739230b4c39ea2a
Lukas Eichler <lukas.eichler@yahoo.com>
2022-07-04 15:12:56 +1000

    restarted

--- project0.js
+++ project0.js
@@ -107,31 +107,5 @@ console.log("hello world");
 
 
 
-let board = [
-    ['X','O','X'],
-    ['O','O','X'],
-    ['X','O','O'],
-];
-
-let player1 = 'X';
-let player2 = 'O';
-
- function setup (){
-    createCanvas(400, 400);
- };
-
- function draw () {
-    background (220);
-    let w = width / 3;
-    let h = height / 3;
-    for (let i = 0; i < 3; i++) {
-        for (let j = 0; j < 3; j++) {
-            let x = w * i;
-            let y = h * i; 
-            let spot = board[i][j];
-            text (spot, x, y);
-        }
-    }
- }
 
 
